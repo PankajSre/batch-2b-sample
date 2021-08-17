3a9235d75fdbcc3743a358352c6a6b8dc16a5c31
PankajSre <pankaj.saini85@gmail.com>
2021-08-17 11:29:24 +0530

    Sample repo

---
+++ Department.java
@@ -0,0 +1,6 @@
+public class Department {
+    public static void main(String[] args){
+	    System.out.println("Hello from the Department");
+		System.out.println("Hello from the Department Again");
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Employee.java  & Department.java Programs
+
+## To Run this, clone the repository in Local Machine
+
+### javac <class-name>.java
+
+### java <class-name>
